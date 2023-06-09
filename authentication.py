7f8e20c0c2f5690a72330652009b05fb3a5cb115
Joseph Vishal <84071740+jo780-full@users.noreply.github.com>
2023-06-09 12:51:49 +0530

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,14 +1,7 @@
 import pyrebase
 
 config={
-      'apiKey': "AIzaSyA_xLGCT3PKw-FhqLnPaoCN1rbPqw1Yki0",
-  'authDomain': "securityapps-17dd1.firebaseapp.com",
-  'projectId': "securityapps-17dd1",
-  'storageBucket': "securityapps-17dd1.appspot.com",
-  'messagingSenderId': "623304704833",
-  'appId': "1:623304704833:web:68aa81bea85c7742b817c0",
-  'measurementId': "G-79RVK93615",
- 'databaseURL': " "
+
 }
 user="testing@gmail.com"
 password="1234567"
